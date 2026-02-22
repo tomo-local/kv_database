17da2cd88c297f6037192ee2e74fa9499c668c69
tomo-local <tomohiro.togashi@gmail.com>
2026-02-23 02:36:09 +0900

    fix: improve error handling in main functions

--- app/main.go
+++ app/main.go
@@ -22,22 +22,24 @@ func main() {
 
 		if err := cmd.Parse(args); err != nil {
 			fmt.Printf("error: %s\n", err)
-			return
+			os.Exit(1)
 		}
 
 		orgs := cmd.Args()
 
 		if len(orgs) < 1 {
 			fmt.Printf("expect more data: %v\n", orgs)
-			return
+			os.Exit(1)
 		}
 
 		key := orgs[0]
 		val, ok, err := kv.Get([]byte(key))
 		if !ok {
 			fmt.Printf("get error: %v\n", err)
+			os.Exit(1)
 		}
 		fmt.Printf("key:%s, value:%s\n", key, string(val))
+		os.Exit(0)
 	}
 
 	set := func(kv KV, args []string) {
@@ -45,22 +47,22 @@ func main() {
 
 		if err := cmd.Parse(args); err != nil {
 			fmt.Printf("error: %s\n", err)
-			return
+			os.Exit(1)
 		}
 
 		orgs := cmd.Args()
 
 		if len(orgs) < 2 {
 			fmt.Printf("expect more data: %v", orgs)
-			return
+			os.Exit(1)
 		}
 
 		key := orgs[0]
 		val := orgs[1]
 
 		kv.Set([]byte(key), []byte(val))
-
 		fmt.Printf("key: %s value: %s\n", key, val)
+		os.Exit(0)
 	}
 
 	del := func(kv KV, args []string) {
@@ -68,32 +70,35 @@ func main() {
 
 		if err := cmd.Parse(args); err != nil {
 			fmt.Printf("error: %s\n", err)
-			return
+			os.Exit(1)
 		}
 
 		orgs := cmd.Args()
 
 		if len(orgs) < 1 {
 			fmt.Printf("expect more data: %v\n", orgs)
-			return
+			os.Exit(1)
 		}
 
 		key := orgs[0]
 		if ok, err := kv.Del([]byte(key)); !ok {
 			fmt.Printf("delete error:  %v\n", err)
+			os.Exit(1)
 		}
 
 		fmt.Println("delete success!")
+		os.Exit(0)
 	}
 
 	list := func(kv KV, _ []string) {
 		data, err := kv.List()
 		if err != nil {
 			fmt.Printf("list error: %v", err)
-			return
+			os.Exit(1)
 		}
 
 		fmt.Println(string(data))
+		os.Exit(0)
 	}
 
 	database := KV{}
@@ -103,8 +108,8 @@ func main() {
 	args := flag.Args()
 
 	if len(args) < 1 {
-		fmt.Printf("expect more data size:%d\n", len(args))
-		return
+		fmt.Printf("Error: expect more data size:%d\n", len(args))
+		os.Exit(1)
 	}
 
 	database.Open()
@@ -120,7 +125,8 @@ func main() {
 	case "list":
 		list(database, args[1:])
 	default:
-		fmt.Println("error")
+		fmt.Println("Error: unknown params")
+		os.Exit(1)
 	}
 
 }
